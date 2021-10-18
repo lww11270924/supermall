4c4514351e9b2fd37d6fa0f526ecd2ac088defb9
lww11270924 <11270924>
2021-10-18 17:59:13 +0800

    提交首页修改

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,42 +5,132 @@
     </nav-bar>
     <HomeSwiper :banners="banners" />
     <RecommendView :recommends="recommend" />
+    <Feature/>
+    <tab-control :titles="['流行','新款','精选']" class="tab-control" />
+
+    <ul>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li><li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+
+    </ul>
+
+
   </div>
 </template>
 
 <script>
-import NavBar from "components/common/navbar/NavBar";
 import HomeSwiper from "./childComps/HomeSwiper";
 import RecommendView from "./childComps/RecommendView";
+import Feature from "./childComps/Feature";
 
-import {getHomrMultidata} from "network/home";
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabControl/tabControl";
+
+import {getHomrMultidata,getHomeGoods} from "network/home";
 
 export default {
   name: "Home",
   components:{
-    NavBar,
     HomeSwiper,
-    RecommendView
+    RecommendView,
+    Feature,
+    NavBar,
+    TabControl
   },
   data(){
     return{
       banners:[],
-      recommend:[]
+      recommend:[],
+      goods:{//流行，新款，精选
+        'pop':{page:0,list:[]},
+        'news':{page:0,list:[]},
+        'sell':{page:0,list:[]}
+      }
     }
   },
   created() {
-    //1请求多个数据
-    getHomrMultidata().then(res => {
-      this.banners = res.data.data.banner.list;
-      this.recommend = res.data.data.recommend.list;
-    })
+    this.getHomrMultidata();
+    this.getHomeGoods();
+  },
+  methods:{
+    getHomrMultidata() {
+      //1请求多个数据
+      getHomrMultidata().then(res => {
+        this.banners = res.data.data.banner.list;
+        this.recommend = res.data.data.recommend.list;
+      })
+    },
+    getHomeGoods(){
+      //2、请求商品信息
+      getHomeGoods('pop',1).then(res => {
+
+      })
+    }
   }
 }
 </script>
 
 <style scoped>
+#home{
+  padding-top: 44px;
+}
 .home-nav{
   background-color: var(--color-tint);
   color: white;
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  z-index: 9;
+}
+
+.tab-control{
+  position: sticky;
+  top:44px
 }
 </style>
