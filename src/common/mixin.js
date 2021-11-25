6d1ad180637c83f0545554b298cb1266f9f04a83
lww11270924 <11270924>
2021-11-25 18:04:39 +0800

    初步添加分类功能

--- src/common/mixin.js
+++ src/common/mixin.js
@@ -1,4 +1,6 @@
 import BackTop from "components/content/backTop/BackTop";
+import {POP, NEW, SELL} from "./const";
+
 export const backTopmixin = {
   data(){
     return{
@@ -11,6 +13,30 @@ export const backTopmixin = {
   methods:{
     backClick() {
       this.$refs.scroll.scrollTo(0,0,500);
+    }
+  }
+}
+
+export const tabControlMixin = {
+  data: function () {
+    return {
+      currentType: POP
+    }
+  },
+  methods: {
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = POP
+          break
+        case 1:
+          this.currentType = NEW
+          break
+        case 2:
+          this.currentType = SELL
+          break
+      }
+      console.log(this.currentType);
     }
   }
 }
